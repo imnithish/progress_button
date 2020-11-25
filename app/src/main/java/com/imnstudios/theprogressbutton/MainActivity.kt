893e7ef2a672d275970d7115b0c1e3fa3ec6e7d5
imnithish <nitheesh@riafy.me>
2020-11-26 01:57:36 +0530

    added new functionalities

--- app/src/main/java/com/imnstudios/theprogressbutton/MainActivity.kt
+++ app/src/main/java/com/imnstudios/theprogressbutton/MainActivity.kt
@@ -6,16 +6,28 @@ import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    var i = 1
+    var f = 17f
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        hella.text = "sfa"
 
-        hella.setOnClickListener {
-            hella.showProgress()
-            Toast.makeText(this, "hella", Toast.LENGTH_SHORT).show()
+
+        login_button.setOnClickListener {
+            login_button.text= "Button was clicked!"
+            login_button.backgroundColor = "#FF0000"
+            login_button.textSize = f++
+            i++
+            login_button.progressBarTint = "#FFFFFF"
+//            login_button.showProgress()
+
         }
+
+//        baseline.setOnClickListener {
+//            login_button.hideProgress()
+//        }
 
     }
 }
--- app/src/main/java/com/imnstudios/theprogressbutton/ProgressButtonTest.kt
+++ app/src/main/java/com/imnstudios/theprogressbutton/ProgressButtonTest.kt
@@ -1,4 +1,4 @@
-package com.imnstudios.library
+package com.imnstudios.theprogressbutton
 
 import android.content.Context
 import android.content.res.ColorStateList
@@ -12,7 +12,7 @@ import android.widget.ProgressBar
 import android.widget.RelativeLayout
 import androidx.core.content.res.ResourcesCompat
 
-class ProgressButton(context: Context, attrs: AttributeSet) : RelativeLayout(context, attrs) {
+class ProgressButtonTest(context: Context, attrs: AttributeSet) : RelativeLayout(context, attrs) {
 
     private var progressBar: ProgressBar? = null
     private var button: Button? = null
@@ -53,41 +53,41 @@ class ProgressButton(context: Context, attrs: AttributeSet) : RelativeLayout(con
 
         val inflater = context
             .getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
-        val rootView = inflater.inflate(R.layout.spinner_button, this, true)
+        val rootView = inflater.inflate(R.layout.spinner_button_test, this, true)
 
         button = rootView.findViewById(R.id.button)
         progressBar = rootView.findViewById(R.id.progress)
 
         val a = context.obtainStyledAttributes(
             attrs,
-            R.styleable.ProgressButton
+            R.styleable.ProgressButtonTest
         )
         val backgroundColor =
-            a.getString(R.styleable.ProgressButton_buttonBackgroundColor) ?: "#BBBDCC"
-        val textColor = a.getString(R.styleable.ProgressButton_textColor) ?: "#000000"
+            a.getString(R.styleable.ProgressButtonTest_buttonBackgroundColor) ?: "#BBBDCC"
+        val textColor = a.getString(R.styleable.ProgressButtonTest_textColor) ?: "#000000"
         setBackgroundColor(Color.parseColor(backgroundColor))
         button?.apply {
-            text = a.getString(R.styleable.ProgressButton_text) ?: ""
+            text = a.getString(R.styleable.ProgressButtonTest_text) ?: ""
             setTextColor(Color.parseColor(textColor))
             setTextSize(
                 TypedValue.COMPLEX_UNIT_SP,
-                a.getFloat(R.styleable.ProgressButton_textSize, 17f)
+                a.getFloat(R.styleable.ProgressButtonTest_textSize, 17f)
             )
-            if (a.getBoolean(R.styleable.ProgressButton_textFontFlag, false))
+            if (a.getBoolean(R.styleable.ProgressButtonTest_textFontFlag, false))
                 typeface = ResourcesCompat.getFont(
                     context,
-                    a.getResourceId(R.styleable.ProgressButton_textFont, R.font.poppins_bold)
+                    a.getResourceId(R.styleable.ProgressButtonTest_textFont, R.font.poppins)
                 )
         }
         progressBar?.indeterminateTintList = ColorStateList.valueOf(
             Color.parseColor(
-                a.getString(R.styleable.ProgressButton_progressBarTint) ?: "#FFFFFF"
+                a.getString(R.styleable.ProgressButtonTest_progressBarTint) ?: "#FFFFFF"
             )
         )
 
 //        progressBar?.layoutParams = LayoutParams(
-//            a.getInteger(R.styleable.ProgressButton_progressBarSize, 35),
-//            a.getInteger(R.styleable.ProgressButton_progressBarSize, 35)
+//            a.getInteger(R.styleable.ProgressButtonTest_progressBarSize, 35),
+//            a.getInteger(R.styleable.ProgressButtonTest_progressBarSize, 35)
 //        )
 
         a.recycle()
